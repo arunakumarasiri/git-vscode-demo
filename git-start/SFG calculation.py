78b1cb6ccdd98e466ef21d085ba0859414b9c866
arunakumarasiri <aruna.pr@outlook.com>
2021-09-14 20:40:14 -0700

    New commit

---
+++ git-start/SFG calculation.py
@@ -0,0 +1,8 @@
+C = 2.998 * 10**8
+IR_f = input("Please input IR frequency in cm-1:\n")
+IR_f = int(IR_f) * C * 10**2
+vis_f = C/ (532* 10**-9)
+SFG_f = IR_f + vis_f
+#IR_wave = C/IR_f
+SFG_wave = (C / SFG_f)*10**9 
+print (f'The corresponding SFG wavelength is {SFG_wave} nm')
